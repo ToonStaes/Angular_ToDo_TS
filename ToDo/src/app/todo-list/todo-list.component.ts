cfe4717f68d0eaef05f0c007f1fc6db5edb22ab8
Toon Staes <r0784094@student.thomasmore.be>
2021-11-22 19:35:36 +0100

    update works completely

--- ToDo/src/app/todo-list/todo-list.component.ts
+++ ToDo/src/app/todo-list/todo-list.component.ts
@@ -6,6 +6,7 @@ import { ItemFormComponent } from '../item-form/item-form.component';
 import { Todo_item } from '../todo-item';
 import * as moment from 'moment';
 import { TodoItemService } from '../todo-item.service';
+import { ListFormComponent } from '../list-form/list-form.component';
 
 @Component({
   selector: 'app-todo-list',
@@ -25,6 +26,7 @@ export class TodoListComponent implements OnInit {
   @Output() addItemEvent: EventEmitter<any> = new EventEmitter();
   @Output() editItemEvent: EventEmitter<any> = new EventEmitter();
   @Output() deleteListEvent: EventEmitter<any> = new EventEmitter();
+  @Output() editListEvent: EventEmitter<any> = new EventEmitter();
 
   todoItemEdit?: Todo_item;
 
@@ -33,17 +35,31 @@ export class TodoListComponent implements OnInit {
   deleteList(list: Todo_list) {
     console.log('delete list with id: ' + list.id);
     this.todolistService.deleteList(list.id).subscribe((result) => {
-      this.deleteListEvent.emit(list)
+      this.deleteListEvent.emit(list);
     });
   }
 
-  editList(id: number) {
-    console.log('edit list with id: ' + id);
+  editList(list: Todo_list) {
+    console.log('edit list with id: ' + list.id);
+    const dialogRef = this.dialog.open(ListFormComponent, {
+      width: '250px',
+      data: { name: list.name, colour: list.category, kind: 'edit' },
+    });
+
+    dialogRef.afterClosed().subscribe((formResult) => {
+      list.name = formResult.name;
+      list.category = formResult.colour;
+
+      this.todolistService.putList(list.id, list).subscribe((result) => {
+        console.log(result)
+        this.editListEvent.emit(result)
+      });
+    });
   }
 
   addItem(listId: number) {
     this.todolistService.getTodoListById(listId).subscribe((result) => {
-      this.list = result;
+      // this.list = result;
 
       const dialogRef = this.dialog.open(ItemFormComponent, {
         width: '450px',
@@ -51,16 +67,23 @@ export class TodoListComponent implements OnInit {
       });
 
       dialogRef.afterClosed().subscribe((formResult) => {
-        formResult.date = moment(formResult.date).format('DD/MM/YYYY');
+        let deadline_approaching = false;
+        var oneweek = moment().add(7, 'days');
+        var itemDate = moment(formResult.date, 'DD/MM/YYYY');
 
+        if (itemDate <= oneweek) {
+          deadline_approaching = true;
+        } else {
+          deadline_approaching = false;
+        }
         let inputItem: Todo_item = {
           description: formResult.description,
-          listId: this.list.id,
+          listId: listId,
           isFinished: false,
           isImportant: false,
-          date: formResult.date,
+          date: itemDate.format('DD/MM/YYYY'),
           id: 0,
-          deadline_approaching: false,
+          deadline_approaching: deadline_approaching,
         };
 
         this.todoItemService.postItem(inputItem).subscribe(
